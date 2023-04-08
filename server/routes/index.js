a9378de78223df8562ee09d0992d42805b7933e6
Jeffery Patterson <jpatterson933@ucla.edu>
2023-04-07 23:21:31 -0700

    1. Post route works to create a new hero.

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,8 @@ const apiRoutes = require("./api");
 // instantantiate api route
 router.use("/api", apiRoutes);
 
-router.use((req, res) => {
-    return res.send("Wrong Routes? Need to test this 4/7")
-});
+// router.use((req, res) => {
+//     return res.send("Wrong Routes? Need to test this 4/7")
+// });
 
 module.exports = router;
